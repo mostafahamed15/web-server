7bb356be9145256b84a128f11412fc62695fabd0
Mostafa <mostafa.hamed@linux-plus.com>
2019-06-16 14:58:27 +0200

    Weather app with form finished

--- public/js/app.js
+++ public/js/app.js
@@ -7,20 +7,26 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 })
 
 
+// Weather form
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 weatherForm.addEventListener('submit', (e)=> {
     e.preventDefault();
     const location = search.value;
-
+    messageOne = 'Loading...';
+    messageTwo = '';
     
 fetch('http://localhost:3000/weather?address=' + location).then((response)=> {
     response.json().then((data)=> {
         if(data.error) {
-            console.log(data.error);
+            messageOne.textContent = data.error;
+
         } else {
-            console.log(data.location);
-            console.log(data.forcast);
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forcast;
+
         }
     })
 })
